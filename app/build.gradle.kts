2b48aa1ef8ca4cbe31cfecd356705d851cc67793
Alex <panitroll33@gmail.com>
2026-03-15 11:30:42 +0100

    up version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ configure<ApplicationExtension> {
         applicationId = "com.botpa.turbophotos"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.1"
     }
 
     buildTypes {
